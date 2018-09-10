f2a1d149f373afb8e75be4527a0211e77fd2b3e3
Your Name <you@example.com>
2018-09-10 16:25:53 -0400

    new boostrap file. modded repo in ARM template.

---
+++ deploy/bootstrap.sh
@@ -0,0 +1,14 @@
+# create a new conda environment with Python 3.6, numpy and cython
+conda create -n azml Python=3.6 cython numpy
+
+# Activate the conde environment
+source activate azml
+
+# install azure-cli
+pip install azure-cli sklearn tensorflow-gpu cntk-gpu keras matplotlib pandas seaborn livelossplot
+
+# install or update azureml meta-package
+pip install --upgrade azureml-sdk
+
+# add azml conda environment as a new Jupyter Kernel
+python -m ipykernel install --user --name azml --display-name "AzureML"
