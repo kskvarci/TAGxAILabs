d4d1733e7f2cc1891cec79ffc212742fd6ba42c4
Your Name <you@example.com>
2018-09-10 21:26:47 -0400

    updated bootstrap

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-set -x #echo on
+# Install the custom vision module for labs 1 and 2
+pip install azure-cognitiveservices-vision-customvision
 
-# create a new conda environment with Python 3.6, numpy and cython
-conda create -n azml Python=3.6 cython numpy
+# Install TA-Lib and talib
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+sudo ./configure
+sudo make
+sudo make install
 
-# Activate the conde environment
+# Create a new kernel for lab 3
+conda create -n azml Python=3.6 cython numpy -y
 source activate azml
-
-# install azure-cli
-pip install azure-cli sklearn tensorflow-gpu cntk-gpu keras matplotlib pandas seaborn livelossplot
-
-# install or update azureml meta-package
+pip install azure-cli sklearn tensorflow-gpu cntk-gpu keras matplotlib pandas seaborn livelossplot ta-lib
 pip install --upgrade azureml-sdk
-
-# add azml conda environment as a new Jupyter Kernel
 python -m ipykernel install --user --name azml --display-name "AzureML"
