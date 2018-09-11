70ddd666a66b7640393099b6405919b650e07c13
Your Name <you@example.com>
2018-09-11 17:29:01 -0400

    minor mods to casing

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -23,11 +23,3 @@ pip install ta-lib
 
 # Install the AzureML meta-package
 pip install --upgrade --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-release/Preview/E7501C02541B433786111FE8E140CAA1 azureml-sdk --ignore-installed pyYAML
-
-# Create a new kernel for lab 3
-#env
-#conda create -n azml Python=3.6 cython numpy -y
-#source activate azml
-#pip install azure-cli sklearn tensorflow-gpu cntk-gpu keras matplotlib pandas seaborn livelossplot ta-lib
-#pip install --upgrade azureml-sdk
-#python -m ipykernel install --user --name azml --display-name "AzureML"
